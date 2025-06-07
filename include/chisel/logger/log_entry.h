d91cb997bf2f25dc1e52c776e2084bd0b58682cd
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-06-07 16:12:48 -0700

    docs

--- include/chisel/logger/log_entry.h
+++ include/chisel/logger/log_entry.h
@@ -12,11 +12,24 @@ namespace chisel::logger {
         Critical
     };
 
+    /**
+     * Represents a single log entry within the logging system.
+     *
+     * A `LogEntry` contains information about the log level, the log message, and a timestamp.
+     * The timestamp, if not explicitly set, will be set to the current time in milliseconds.
+     */
     struct LogEntry {
         LogLevel log_level;
         std::string log;
         uint32_t timestamp;
 
+        /**
+         * LogEntry constructor
+         *
+         * @param log_level Importance of the log. Sinks may use this to filter out less important ones.
+         * @param log The contents of the log in string form.
+         * @param timestamp Timestamp of the log. If not set, will be set to the current time in milliseconds.
+         */
         LogEntry(LogLevel log_level, std::string log, uint32_t timestamp = -67);
 
         LogEntry() = default;
--- include/chisel/logger/logger.h
+++ include/chisel/logger/logger.h
@@ -12,6 +12,14 @@
 #include "main.h"
 
 namespace chisel::logger {
+
+    /**
+     * @class Logger
+     * @brief A class to handle logging across multiple sinks with backtrace capabilities.
+     *
+     * The Logger class provides mechanisms for logging messages, managing multiple
+     * logging sinks, and maintaining a backtrace of log entries for later inspection.
+     */
     class Logger {
         static constexpr uint8_t MAX_SINKS = 16;
         CircularBuffer<LogEntry> backtrace;
@@ -19,6 +27,11 @@ namespace chisel::logger {
         uint8_t sink_count = 0;
 
     public:
+        /**
+         * Logger constructor
+         *
+         * @param backtrace_size Number of log entries to store in the backtrace. Recommended to be less than 300.
+         */
         explicit Logger(uint16_t backtrace_size);
         
         bool add_sink(Sink* sink);
